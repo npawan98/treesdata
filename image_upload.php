5781b04d304fa60caa02067497b34701607889de
npawan98 <npawan98@gmail.com>
2021-07-03 22:50:02 +0530

    modified (working)

--- image_upload.php
+++ image_upload.php
@@ -2,13 +2,27 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$dbname = "temp";
+$dbname = "treedata";
 $conn = new PDO("mysql:host=$server;dbname=$dbname","$username","$password");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $targetDir = "photos/";
 $image = $_FILES['files']['name'];
 $fileName = implode(",",$image);
-$username = $_REQUEST['name'];
+
+
+$Scientific_name = $_REQUEST['Scientific_name'];
+$Common_name = $_REQUEST['Common_name'];
+$Family_name = $_REQUEST['Family_name'];
+$Kannada_name = $_REQUEST['Kannada_name'];
+$Age = $_REQUEST['Age'];
+$Description = $_REQUEST['Description'];
+$Girth = $_REQUEST['Girth'];
+$Height = $_REQUEST['Height'];
+$Tree_Health_Condition = $_REQUEST['Tree_Health_Condition'];
+$Rating = $_REQUEST['Rating'];
+$Latitude_and_Longitude = $_REQUEST['Latitude_and_Longitude'];
+$any_other_details = $_REQUEST['any_other_details'];
+// $images = $_REQUEST['images'];
 // echo $fileName;
 
 if(!empty($image)){
@@ -16,12 +30,10 @@ if(!empty($image)){
         $targetFilePath = $targetDir . $val;
         move_uploaded_file($_FILES['files']['tmp_name'][$key],$targetFilePath);
     }
-    $query = "INSERT INTO images(image_name,name) VALUES('$fileName','$username')";
+    // $query = "INSERT INTO images(image_name,name) VALUES('$fileName','$username')";
+    $query = "INSERT INTO treedetails VALUES ('$Scientific_name','$Common_name','$Family_name','$Kannada_name','$Age','$Description','$Girth','$Height','$Tree_Health_Condition','$Rating','$Latitude_and_Longitude','$any_other_details','$fileName','')";
     $statement = $conn->prepare($query);
     $statement->execute();
     echo "Images Uploaded";
 };
-
-
-
 ?>
--- index.php
+++ index.php
@@ -4,16 +4,77 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>Trees Data</title>
 </head>
-<body>
+<body style="margin:25px">
 
     <form action="" method="post" id="form">
+    
+        <div class="form-group">
+            <label for="text">Scientific name</label>
+            <input type="text" class="form-control" name="Scientific_name">
+        </div>
+        <div class="form-group">
+            <label for="text">Common name</label>
+            <input type="text" class="form-control" name="Common_name">
+        </div>
+        <div class="form-group">
+            <label for="text">Family name </label>
+            <input type="text" class="form-control" name="Family_name">
+        </div>
+        <div class="form-group">
+            <label for="text">Kannada name</label>
+            <input type="text" class="form-control" name="Kannada_name">
+        </div>
+        <div class="form-group">
+            <label for="text">Age</label>
+            <input type="text" class="form-control" name="Age">
+        </div>
+        <div class="form-group">
+            <label for="text">Description</label>
+            <input type="text" class="form-control" name="Description">
+        </div>
+        <div class="form-group">
+            <label for="text">Girth</label>
+            <input type="text" class="form-control" name="Girth">
+        </div>
+        <div class="form-group">
+            <label for="text">Height</label>
+            <input type="text" class="form-control" name="Height">
+        </div>
+        <div class="form-group">
+            <label for="text">Tree Health Condition</label>
+            <input type="text" class="form-control" name="Tree_Health_Condition">
+        </div>
+        <div class="form-group">
+            <label for="text">Rating</label>
+            <input type="text" class="form-control" name="Rating">
+        </div>
+        <div class="form-group">
+            <label for="text">Latitude and Longitude </label>
+            <input type="text" class="form-control" name="Latitude_and_Longitude">
+        </div>
+        <div class="form-group">
+            <label for="text">any other details</label>
+            <input type="text" class="form-control" name="any_other_details">
+        </div>
+        <!-- <div class="form-group">
+             <label for="pwd">Password:</label>
+             <input type="password" class="form-control" id="pwd">
+        </div> -->
+
+
         <input type="file" name="files[]" multiple/>
-        <input type="text" name="name"/>
+        <!-- <input type="text" name="name"/> -->
         <input type="submit" value="submit"/>
+
     </form>
+
+
+
     <script src="jquery.min.js"></script>
     
      <script>
